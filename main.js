b4b83b146da048d813a9b2252006f396aad71d64
BunyodbekOlimov <oybekivichbunyod@gmail.com>
2024-06-07 02:29:02 +0500

    start

---
+++ main.js
@@ -0,0 +1,160 @@
+{
+    // 1-masala
+
+    let x = +prompt("x ga son kirting")
+    if(x > 0){
+        x++
+        console.log(x);
+    }else{
+        console.log(x);
+    }
+    alert (` ${x}`)
+}
+
+
+// {
+//     // 2-masala
+
+//     let x = +prompt("x ga son kirting")
+//     if(x > 0){
+//         x++
+//         console.log(x);
+//     }else if(x < 0){
+//         x = x - 1
+//         console.log(x);
+//     }
+//     alert (`${x}`)
+// }
+
+
+// {
+//     // 3-masala
+
+//     let x = +prompt("x ga son kirting")
+//     if(x > 0){
+//         x++
+//         console.log(x);
+//     }else if(x < 0){
+//         x = x - 2
+//     }else if(x === 0){
+//         x = 10
+//     }
+//     alert (`${x}`)
+// }
+
+
+
+// {
+//     // 4-masala
+
+//     let x1 = +prompt("x1 ga son kirting")
+//     let x3 = +prompt("x1 ga son kirting")
+//     let x2 = +prompt("x1 ga son kirting")
+//     let musbat = 0
+//     if(x1 > 0){
+//         musbat++
+//     }
+//     if(x2 > 0){
+//         musbat++
+//     }
+//     if(x3 > 0){
+//         musbat++
+//     }
+//     alert (`${musbat}`)
+// }
+
+
+
+// {
+//     // 5-masala
+
+//     let x1 = +prompt("x1 ga son kirting")
+//     let x2 = +prompt("x2 ga son kirting")
+//     let x3 = +prompt("x3 ga son kirting")
+//     let musbat = 0
+//     let manfiy = 0
+//     if(x1 > 0){
+//         musbat++
+//     }else if(x1 < 0){
+//         manfiy++
+//     }
+//     if(x2 > 0){
+//         musbat++
+//     }else if(x2 < 0){
+//         manfiy++
+//     }
+//     if(x3 > 0){
+//         musbat++
+//     }else if(x3 < 0){
+//         manfiy++
+//     }
+//     alert(`musbat son: ${musbat} manfiy son: ${manfiy}`)
+// }
+
+
+
+// {
+//     // 6-masala
+
+//     let a = +prompt("a ga son kirting")
+//     let b = +prompt("b ga son kiritng")
+//     if(a > b){
+//         alert(`a son katta: ${b}`)
+//     }else if(a < b){
+//         alert(` b son katta: ${b}`)
+//     }
+// }
+
+
+// {
+//     // 7-masala
+
+//     let x1 = +prompt("x1 ga son kirting")
+//     let x2 = +prompt("x2 ga son kirting")
+//     if(x1 > x2){
+//         alert(`x2 kichik`)
+//     }else if(x1 < x2){
+//         alert(`x1 kichik`)
+//     }
+// }
+
+
+// {
+//     // 8-masala
+
+//     let x1 = +prompt("x1 ga son kirting")
+//     let x2 = +prompt("x2 ga son kirting")
+//     if(x1 > x2){
+//         alert (`kattasi:${x1} kichigi: ${x2}`)
+//     }else {
+//         alert (`kattasi: ${x2} kichigi: ${x1}`)
+//     }
+// }
+
+
+// {
+//     // 9-masala
+
+//     let A = +prompt("A ga son kirting")
+//     let B = +prompt("B ga son kirting")
+//     if(A < B){
+//         alert (`${A} son ${B} dan kichik`)
+//     }
+// }
+
+
+// {
+//     // 10-masala
+
+//     let a = +prompt("a sonini kirting")
+//     let b = +prompt("b sonini kirting")
+//     let sum = 0
+//     if(a!=b){
+//         sum = a + b
+//         alert(`${sum}`)
+//     }else{
+//         a = 0
+//         b = 0
+//         alert(`${a} ${b}`)
+//     }
+// }
